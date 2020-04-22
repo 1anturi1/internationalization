30977e8fee7f76804d485701da0a37b004e3484c
1anturi1 <ai.anturi@uniandes.edu.co>
2020-04-22 17:14:54 -0500

    Retos realizados

--- src/components/jobsList.js
+++ src/components/jobsList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Job from "./job";
+import { FormattedMessage } from 'react-intl';
 
 export default class JobsList extends React.Component {
-
+/**navigator.language||navigator.browserLanguage */
   state = { 
+    "color" : "black" ,
   	"offers": [
     	{
     	  "id": "0001",
@@ -11,7 +13,8 @@ export default class JobsList extends React.Component {
     	  "company": "Schneider Electric", 
     	  "salary": 4.5,
     	  "city": "Bogotá, Colombia",
-        "date": "2019-03-26"
+        "date": "2019-03-26",
+        "numVisits": 134
       }, 
       {
         "id": "0002",
@@ -19,7 +22,8 @@ export default class JobsList extends React.Component {
         "company": "Google Inc.", 
         "salary": 20,
         "city": "Palo Alto, CA, USA",
-        "date": "2019-03-27"
+        "date": "2019-03-27",
+        "numVisits": 1340
       },
       {
         "id": "0003",
@@ -27,25 +31,30 @@ export default class JobsList extends React.Component {
         "company": "Clínica La Aurora", 
         "salary": 1,
         "city": "Cali, Colombia",
-        "date": "2019-03-28"
+        "date": "2019-03-28",
+        "numVisits": 13420
       }
     ]
   };
 
   render() {
     return (
-      <div>
+      <div>        
         <table className="table">
-          <thead className="thead-dark">
+          <FormattedMessage id = "ColorHead">
+          {(txt) => (
+          <thead className= {txt}>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">Position</th>
-              <th scope="col">Company</th>
-              <th scope="col">Salary</th>
-              <th scope="col">City</th>
-              <th scope="col">Publication date</th>
+              <th scope="col"><FormattedMessage id="Position"/></th>
+              <th scope="col"><FormattedMessage id="Company"/></th>
+              <th scope="col"><FormattedMessage id="Salary"/></th>
+              <th scope="col"><FormattedMessage id="City"/></th>
+              <th scope="col"><FormattedMessage id="PulibicationDate"/></th>
+              <th scope="col"><FormattedMessage id="Views"/></th>
             </tr>
-          </thead>
+          </thead> )}
+          </FormattedMessage>
           <tbody>
               {this.state.offers.map( (e,i) => <Job key={i} offer={e}/>)}
           </tbody>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en"
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+var lang = function() {
+	if( navigator.language.includes("es")){
+		return "es";
+	} 
+	else{
+		return "en";
+	}	
+  }
+
+var messages = function(){	
+	if( lang().includes("es")){
+		return localeEsMessages ;
+	}
+	else{
+		return localeEnMessages;
+	}
+} 
+
+ReactDOM.render(
+	<IntlProvider locale={lang()} messages= {messages()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
